2cecd6265290b880d92a9d7f3ebb34285fe29483
Peter Belcher <peter.belcher@live.com>
2017-01-29 22:40:19 +0000

    added logging and level checking

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
+STILL WORK IN PROGRESS. PLS CHECK BACK SOON
+
 #grovepi-plant-waterer
 
-STILL WORK IN PROGRESS. PLS CHECK BACK SOON
+#TODO
+- Check where grovePI can be installed vs plant water script
+- calibration script
+- add option to log metrics to an api
+
+# Installing grovePI & waterer script
+
+# Calibrating the waterer
+
+# Running the script in the background
+Make the script executable:
+'''python
+chmod +x plant_waterer.py
+'''
+Create a cron job to run the script each time the pi starts:
+'''python
+sudo crontab -e
+'''
+'''python
+@reboot python /home/pi/plant_waterer.py &
+'''
+You can then view the process ID should you need to force quit using:
+'''python
+ps ax | grep test.py
+'''
--- plant_water.py
+++ plant_water.py
@@ -9,36 +9,37 @@
 import time
 import grovepi
 from grove_rgb_lcd import *
+import datetime
+import file
 
+# Set pins for variouse connected grove devices. Disable moist sensors & relay according to how
+# many of them you have configured
 ledbar = 7
 ultrasonic_ranger = 2
 moist_sens_1 = 0
 #moist_sens_2 = 0
 relay_1 = 3
-relay_2 = 4
+#relay_2 = 4
 dht_sensor_port = 8
 dht_sensor_type = 0
 
-grovepi.pinMode(ledbar,"OUTPUT")
-grovepi.pinMode(relay_1,"OUTPUT")
-grovepi.pinMode(relay_2,"OUTPUT")
-setRGB(0,255,0)
-timeSinceWater = 0
-
 # Set your trigger thresholds here. Will require some expermineting depending
-# on the size and depth of your water resovior
-
-##### turn values to int?
-
+# on the size and depth of your water resovior. Run calbiraton script to help you out
 moistureLevelTrigger = 450 # Levelunderwhich water event will trigger
 minWaterLevel = 15 # Minimum water remaining before water events stop
 maxWaterLevel = 150
 minWaterGap = 10 # Minimum interval in mins between triggering a watering cycle
 waterPumpDuration = 2 # Seconds to run pump for
+logging = 1 # Set 1 for enabled, 0 for disabled
 
+#Set GPIO communicaion type on app startup
+grovepi.pinMode(ledbar,"OUTPUT")
+grovepi.pinMode(relay_1,"OUTPUT")
+grovepi.pinMode(relay_2,"OUTPUT")
+lastWateredOne = 0;
+lastWateredTwo = 0;
 time.sleep(1)
 
-### RUN AS A BACKGROUND SCRIPT
 while True:
     try:
         ledLevel = grovepi.ledBar_getBits(ledbar)
@@ -46,24 +47,29 @@ while True:
         moistureOne = grovepi.analogRead(moist_sens_1)
         moistureTwo = grovepi.analogRead(moist_sens_2)
         [ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)
+        current_time = datetime.datetime.now(datetime.timezone.utc)
+        unix_timestamp = current_time.timestamp()
 
-        # TODO - Check if are outside the minWaterGap since last watered - timeSinceWater
-        # Check water level
+        # Check water level. if it is ok, check if we need to pump and do it
         if waterDistance > minWaterGap:
-            # if it is, check if we need to pump and do it
-            if moistureOne < moistureLevelTrigger and moist_sens_1:
-                grovepi.digitalWrite(relay_1,1)
-                time.sleep(waterPumpDuration)
-                grovepi.digitalWrite(relay_1,0)
-            if moistureTwo < moistureLevelTrigger and moist_sens_2:
-                grovepi.digitalWrite(relay_2,1)
-                time.sleep(waterPumpDuration)
-                grovepi.digitalWrite(relay_2,0)
-
+            setRGB(0,255,0)
+            if lastWateredOne > (unix_timestamp - 900):
+                if moistureOne < moistureLevelTrigger and moist_sens_1:
+                    grovepi.digitalWrite(relay_1,1)
+                    time.sleep(waterPumpDuration)
+                    grovepi.digitalWrite(relay_1,0)
+                    lastWateredOne = current_time.timestamp()
+                # TODO - Check if are outside the minWaterGap since last watered - lastWatered
+            if lastWateredTwo > (unix_timestamp - 900):
+                if moistureTwo < moistureLevelTrigger and moist_sens_2:
+                    grovepi.digitalWrite(relay_2,1)
+                    time.sleep(waterPumpDuration)
+                    grovepi.digitalWrite(relay_2,0)
+                    lastWateredTwo = current_time.timestamp()
             #disply time, temp and water level. Update LED bar
             setText("Temp":  + str(temp) + " Hum: " + str(hum))
-
         else:
+            setRGB(255,0,0)
             print "Water level too low to do anything. Log to text file"
             setText("Temp":  + str(temp) + " Hum: " + str(hum) + "WATER TOO LOW!!")
 
@@ -73,8 +79,14 @@ while True:
         grovepi.ledBar_setLevel(ledbar, barlevel)
 
         # Add a logging if. If enabled, write log with run determination
+        if logging > 0 :
+            timestr = time.strftime("%Y%m%d")
+            outputfile = "./logs/" + timestr + ".txt"
+            with open(outputfile, "w") as text_file:
+                print(unix_timestamp + ":" + " Water Level:" + waterDistance + " Moisture Levels:" +
+                    moistureOne + "/" + moistureTwo + " Last Watered:" + lastWateredOne + "/" + lastWateredTwo + "\n")
 
-        #TODO- Set to 15 mins after testing
+        #TODO- Set to 15 mins after testing (900)
         time.sleep(5)
 
     except KeyboardInterrupt:
